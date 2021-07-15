df05cd4a9f5ed764006db823f622f3a0ce7780eb
bojjacharitha <54342502+bojjacharitha@users.noreply.github.com>
2021-07-15 08:05:21 +0530

    Update receiving_code.ino

--- LiFi based navigation system/receiving_code.ino
+++ LiFi based navigation system/receiving_code.ino
@@ -1,43 +1,67 @@
-int flag = 1, ar[200] = {}, a0, i = 0;
+#include <LiquidCrystal.h>
 int ldr = A0;
-int cutoff = 500;
+int dly = 15;
+int cutoff = 300;
+int arr[4];
+int c = 0;
+int lcd_delay = 500;
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 void setup() {
   // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.print("Starting....");
+  delay(1000);
+  lcd.clear();
   Serial.begin(9600);
   pinMode(ldr, INPUT);
 }
 
 void loop() {
+  ///
   // put your main code here, to run repeatedly:
-  if(flag){
-    while(i != 200){
-      ar[i++] = getbit();
-      delay(50);
-      //Serial.println(ar[i-1]);
+  delay(1000);
+  for (int i = 0; i < 4; i++) {
+    arr[i] = rdldr(analogRead(ldr));
+    delay(dly);
+    Serial.println(arr[i]);
+  }
+  int  m = arr[0] + arr[1] + arr[2] + arr[3];
+  if (m == 3) {
+    Serial.println("3");
+    lcd.print("Location:C");
+    lcd.setCursor(0, 0);
+    //  delay(lcd_delay);
+    //  lcd.clear();
+  }
+  else {
+    if (arr[0] != arr[2]) {
+      Serial.println("2");
+      lcd.print("Location:B");
+      lcd.setCursor(0, 0);
+      //    delay(lcd_delay);
+      //  lcd.clear();
     }
-    int a1 = 0, flag1 = 0;
-    
-    for(int j = 0; j<200; j++){
-      for(int k = j; k<(j+8); k++){
-        a1 += (1<<(7-k))*(ar[k]);
-      }
-      if(a1 == 33){
-        flag1 = 1;
-        j = 200;
-      }
+    else if (arr[1] != arr[3]) {
+      Serial.println("2");
+      lcd.print("Location:C");
+      lcd.setCursor(0, 0);
+      //        delay(lcd_delay);
+      //  lcd.clear();
     }
-    if(flag1){
-      Serial.println("YES");
-    }else{
-      Serial.println("NO");
+    else {
+      Serial.println("1");
+      lcd.print("Location:A");
+      lcd.setCursor(0, 0);
+      //        delay(lcd_delay);
+      //  lcd.clear();
     }
-    flag--;
   }
 }
 
-int getbit(){
-  if(analogRead(ldr) > cutoff){
+int rdldr(int d) {
+  if (d < cutoff)
     return 1;
-  }
-  return 0;
+  else
+    return 0;
 }
