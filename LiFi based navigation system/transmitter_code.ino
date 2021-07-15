8285a19c4d28cdf2ef0e2870abb68e4856aa961a
bojjacharitha <54342502+bojjacharitha@users.noreply.github.com>
2021-07-15 08:05:57 +0530

    Update transmitter_code.ino

--- LiFi based navigation system/transmitter_code.ino
+++ LiFi based navigation system/transmitter_code.ino
@@ -1,55 +1,42 @@
-int led = 9;
-int a1, a2;
-int dly = 1000;
+int led = 2;
+int n = 3;
+int dly =15; void setup(){ // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(led,OUTPUT);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(led, OUTPUT);
 }
-int flag = 1;
+
 void loop() {
   // put your main code here, to run repeatedly:
-  String incoming;
-  int b1;
-  char b[9] = "!ABCDEFG!";
-  
-  if(flag == 1){
-      for(int j = 0; j<1; j++){
-          b1 = b[j];
-          int a = b1, ba1 = 0;
-          int ar[8] = {}, k = 0;
-          Serial.println(a); 
-          for(int k = 0; k<8; k++){
-          if(a&1){
-            analogWrite(9, 255);
-            delay(dly);
-            analogWrite(led, 0);
-          }else{
-            analogWrite(led, 0);
-            delay(dly);
-            analogWrite(led, 0);
-          }
-          a >>= 1;
-        /*
-          if(a2 < 600){
-            ar[k++] = 0;
-          }else{
-            ar[k++] = 1;
-          }
-          */
-        }
-
-       /*
-        for(int i = 7; i>=0; i--){
-            Serial.println(ar[i]);
-           ba1 += ((1<<i)*ar[i]);
-        }
-
-        Serial.println(ba1);
-      */
-      }
-  }
-      //a1 = analogRead(A0);
-      //Serial.println(a1);
+  //digitalWrite(led,HIGH);
+if (n==1){
+  digitalWrite(led,HIGH);
+  delay(dly);
+  digitalWrite(led,LOW);
+  delay(dly);
+  digitalWrite(led,HIGH);
+  delay(dly);
+  digitalWrite(led,LOW);
+  delay(dly);
+}
+if (n==2){
+  digitalWrite(led,HIGH);
+  delay(dly);
+  digitalWrite(led,HIGH);
+  delay(dly);
+  digitalWrite(led,LOW);
+  delay(dly);
+  digitalWrite(led,LOW);
+  delay(dly);
+}
+if (n==3){
+  digitalWrite(led,HIGH);
+  delay(dly);
+  digitalWrite(led,HIGH);
+  delay(dly);
+  digitalWrite(led,HIGH);
+  delay(dly);
+  digitalWrite(led,LOW);
+  delay(dly);
+}
 }
